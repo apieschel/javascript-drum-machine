d53aada50018ceb7c645f38764b2787ec67112dc
Glitch (hello-webpage) <none>
2019-02-17 17:53:25 +0000

    ⛵️🎉 Checkpoint ./script.js:2265073/263

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // source: http://drum-machine.maryrosecook.com/
-
 let audio = new AudioContext();
 
-function createSineWave() {
+function createSineWave(audio, duration) {
   let oscillator = audio.createOscillator();
   oscillator.type = "sine";
-}
+  
+  oscillator.start(audio.currentTime);
+  oscillator.stop(audio.currentTime + duration);
+  
+  return oscillator;
+};
+
+function createAmplifier(audio, startValue, duartion) {
+
+};
 
 function note(audio, frequency) {
   return function() {
     let duration = 1;
     let sineWave = createSineWave(audio, duration);
+    
+    sineWave.connect(audio.destination);
   }
-}
+};
 
 note(audio, 440)();
