6a85ebe99e55223f778210188ccf9893612b4f21
Glitch (hello-webpage) <none>
2019-02-17 17:41:39 +0000

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let audio = new AudioContext();
 
+function createSineWave() {
+  let oscillator = audio.createOscillator();
+  oscillator.type = "sine";
+}
+
+function note(audio, frequency) {
+  return function() {
+    let duration = 1;
+    let sineWave = createSineWave(audio, duration);
+  }
+}
+
+note(audio, 440)();
