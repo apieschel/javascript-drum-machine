30e9390ee76b8d8364102787ee2895e00ead650c
Glitch (hello-webpage) <none>
2019-02-21 01:20:04 +0000

    📒⛴ Checkpoint ./script.js:2265073/35

--- script.js
+++ script.js
@@ -147,6 +147,6 @@ setInterval(function() {
     // Source: https://medium.com/@bryanjenningz/how-to-record-and-play-audio-in-javascript-faa1b2b3e49b
     console.log("You're recording!");
     
-    navigator.mediaDevices.getDisplayMedia();
+    navigator.mediaDevices.enumerateDevices();
   });
 })();
